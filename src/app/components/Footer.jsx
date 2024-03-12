fc3eedd5171b0b48b08078be7bf37c7191ad8ef3
sictracr <courier@sictracr.com>
2024-03-12 11:52:47 -0600

    save to upload

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -10,13 +10,15 @@ const Footer = () => {
             bgPosition="center"
             bgRepeat="no-repeat">
                 <Box display="flex"
+                    alignItems="center"
                     w="90%"
                     ml="auto"
-                    mr="auto"
-                    align-items="center">
+                    mr="auto">
                     <Image w="40%" src='/images/sictraLogo.png' alt='el logo de sictra' />
                     <Text w="40%" textAlign="center">San José, Costa Rica - Centroamérica.</Text>
                 </Box>
+                <Box></Box>
+                <Box></Box>
         </Box>
     );
 }
