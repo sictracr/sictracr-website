07055cc20f980fcbe937c7c06b7ab44bf143fef8
sictracr <courier@sictracr.com>
2024-03-20 11:50:40 -0600

    se termina el footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,17 +6,17 @@ const Footer = () => {
     return (
         <Box as="footer" maxW="1480" bgImage="url('/images/footerbg.png')" bgPosition="center" bgRepeat="no-repeat">
             <Box display="flex" alignItems="center" justifyContent="space-between" w="90%" ml="auto" mr="auto">
-                <Image src='/images/sictraLogo.png' alt='el logo de sictra' w="280px" />
-                <Text textAlign="center" mr="60">San José, Costa Rica - Centroamérica.</Text>
+                <Image src='/images/sictraLogo.png' alt='el logo de sictra' w="30%" />
+                <Text textAlign="center" w="60%" fontWeight="bold" fontSize="32" color="#001E40">San José, Costa Rica - Centroamérica.</Text>
             </Box>
-            <Box display="flex" alignItems="center" w="90%" ml="auto" mr="auto" mt="10">
-                <Box>
-                    <Text>Contacto</Text>
+            <Box display="flex" w="90%" ml="auto" mr="auto" mt="10" color="#001E40">
+                <Box w="50%">
+                    <Text fontSize="18" fontWeight="bold" mb="2">Contacto</Text>
                     <FooterContact/>
                 </Box>
-                <Box>
-                    <Text>Soporte</Text>
-                    <Link href='/preguntasfrecuentes'>¿Preguntas Frecuentes?</Link>
+                <Box w="50%">
+                    <Text fontSize="18" fontWeight="bold" mb="2">Soporte</Text>
+                    <Link href='/preguntasfrecuentes' fontSize="14" fontWeight="bold">¿Preguntas Frecuentes?</Link>
                 </Box>
             </Box>
             <Center
--- src/app/components/FooterContact.jsx
+++ src/app/components/FooterContact.jsx
@@ -1,42 +1,43 @@
 "use client"
-import { Box, Text, Center } from "@chakra-ui/react";
+import { Box, Link } from "@chakra-ui/react";
 import { FaPhone } from "react-icons/fa6";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaFacebook } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
 import { HiOutlineMailOpen } from "react-icons/hi";
+import NextLink from 'next/link'
 
 const FooterContact = () => {
     return (
         <Box>
             <Box display="flex">
-                <Box>
-                    <Box display="flex">
-                        <Box as="span"><FaPhone /></Box>
-                        <Box as="span">2221-4558</Box>
+                <Box w="30%">
+                    <Box display="flex" alignItems="center" mb="4">
+                        <Box as="span" mr="2" fontSize="25"><FaPhone /></Box>
+                        <Box as="span" fontSize="14" fontWeight="bold" >2221-4558</Box>
                     </Box>
-                    <Box display="flex">
-                        <Box as="span"><IoLogoWhatsapp /></Box>
-                        <Box as="span">7040-1647</Box>
+                    <Box display="flex" alignItems="center">
+                        <Box as="span" mr="2" fontSize="25"><IoLogoWhatsapp /></Box>
+                        <Box as="span" fontSize="14" fontWeight="bold">7040-1647</Box>
                     </Box>
                 </Box>
-                <Box>
-                    <Box display="flex">
-                        <Box as="span"><FaFacebook /></Box>
-                        <Box as="span">SICTRA CR</Box>
+                <Box w="50%">
+                    <Box display="flex" alignItems="center" mb="4">
+                        <Box as="span" mr="2" fontSize="25"><FaFacebook/></Box>
+                        <Link as={NextLink} href='https://www.facebook.com/profile.php?id=100094634959181&locale=es_LA' fontSize="14" fontWeight="bold">SICTRA CR</Link>
                     </Box>
-                    <Box display="flex">
-                        <Box as="span"><BsInstagram /></Box>
-                        <Box as="span">sictra.cr1</Box>
+                    <Box display="flex" alignItems="center">
+                        <Box as="span" mr="2" fontSize="25"><BsInstagram /></Box>
+                        <Link as={NextLink} href='https://www.instagram.com/sictra.cr1/' fontSize="14" fontWeight="bold">sictra.cr1</Link>
                     </Box>
                 </Box>
             </Box>
-            <Center>
-                <Box display="flex">
-                    <Box as="span"><HiOutlineMailOpen /></Box>
-                    <Box as="span">servicioalcliente@sictracr.com</Box>
+            <Box mt="4">
+                <Box display="flex" alignItems="center">
+                    <Box as="span" mr="2" fontSize="25"><HiOutlineMailOpen /></Box>
+                    <Box as="span" fontSize="14" fontWeight="bold">servicioalcliente@sictracr.com</Box>
                 </Box>
-            </Center>
+            </Box>
         </Box>
     );
 }
